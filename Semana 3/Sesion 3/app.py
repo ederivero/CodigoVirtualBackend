c2cfc696c0a762e5665e69f125c132022f3446ac
Eduardo de Rivero <eduardo.derivero@valigo.org>
2020-11-07 22:55:12 -0500

    avance

--- Semana 3/Sesion 3/app.py
+++ Semana 3/Sesion 3/app.py
@@ -17,5 +17,12 @@ def inicio():
     }]
     return render_template('index.html',clientes=resultado)
 
+@app.route('/acerca-de')
+def acerca_de():
+    return render_template('acerca_de.html')
+
+@app.route('/contactame')
+def contact():
+    return render_template('contactame.html')
 if __name__ == '__main__':
     app.run(debug=True)
