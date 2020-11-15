59b40c18e3ac288c561dc1164af7f54725f830fd
Eduardo de Rivero <eduardo.derivero@valigo.org>
2020-11-14 19:07:53 -0500

    avance

--- Semana 4/canchitas/administracion/models.py
+++ Semana 4/canchitas/administracion/models.py
@@ -12,3 +12,50 @@ class LocalModel(models.Model):
     updatedAt = models.DateTimeField(db_column='updated_at', auto_now=True)
     class Meta:
         db_table = 't_local'
+
+class TipoCanchaModel(models.Model):
+    tipoCanchaId = models.AutoField(db_column='tc_id', primary_key=True, null=False, unique=True)
+    tipoCanchaDesc = models.CharField(db_column='tc_desc', max_length=40)
+    createdAt = models.DateTimeField(db_column='created_at', auto_now_add=True)
+    updatedAt = models.DateTimeField(db_column='updated_at', auto_now=True)
+    class Meta:
+        db_table = 't_tipocancha'
+
+class CanchaModel(models.Model):
+    canchaId = models.AutoField(db_column='cancha_id', primary_key=True, null=False, unique=True)
+    canchaNomb = models.CharField(db_column='cancha_nom', max_length=40)
+    canchaPrecio = models.DecimalField(db_column='cancha_prec', max_digits=5, decimal_places=2)
+    canchaEstado = models.BooleanField(db_column='cancha_est', default=True)
+    tipoCanchaId = models.ForeignKey(TipoCanchaModel, on_delete=models.PROTECT, db_column='tc_id', related_name='canchasTipoCancha')
+    localId = models.ForeignKey(LocalModel, on_delete=models.PROTECT, db_column='local_id', related_name='canchasLocal')
+    createdAt = models.DateTimeField(db_column='created_at', auto_now_add=True)
+    updatedAt = models.DateTimeField(db_column='updated_at', auto_now=True)
+    class Meta:
+        db_table = 't_cancha'
+
+class TipoClienteModel(models.Model):
+    tipoClienteId = models.AutoField(db_column='tc_id', primary_key=True, null=False, unique=True)
+    tipoClientePrec = models.DecimalField(db_column='tc_prec', max_digits=5,decimal_places=2)
+    createAt = models.DateTimeField(db_column='create_at',auto_now_add=True)
+    updateAt = models.DateTimeField(db_column='update_at',auto_now=True)
+    class Meta:
+        db_table='t_tipocliente'
+
+class ClienteModel(models.Model):
+    clienteId = models.AutoField(db_column='cliente_id', primary_key=True, null=False, unique=True)
+    clienteNombre = models.CharField(db_column='cliente_nombre', max_length=40, null=False)
+    tipoClienteId = models.ForeignKey(TipoClienteModel, on_delete=models.PROTECT, db_column='tc_id', related_name='clientesTipo')
+    createAt = models.DateTimeField(db_column='create_at',auto_now_add=True)
+    updateAt = models.DateTimeField(db_column='update_at',auto_now=True)
+    class Meta:
+        db_table = 't_cliente'
+
+class RegistroModel(models.Model):
+    registroId = models.AutoField(db_column='reg_id', primary_key=True, null=False, unique=True)
+    registroPrecFinal = models.DecimalField(db_column='reg_precfin', max_digits=5, decimal_places=2)
+    registroFechIni = models.DateTimeField(db_column='reg_fechin')
+    registroFechFin = models.DateTimeField(db_column='reg_fechfin')
+    canchaId = models.ForeignKey(CanchaModel, on_delete=models.PROTECT, db_column='cancha_id', related_name='registrosCancha')
+    clienteId = models.ForeignKey(ClienteModel, on_delete=models.PROTECT, db_column='cliente_id', related_name='registrosCliente')
+    class Meta:
+        db_table = 't_registro'
