242c6179a7b0a9be43278af9f8429ca0802c822d
Eduardo de Rivero <eduardo.derivero@valigo.org>
2020-12-05 19:10:03 -0500

    dia 3 semana 7

--- Semana 7/MongooseTs/controllers/video.ts
+++ Semana 7/MongooseTs/controllers/video.ts
@@ -5,6 +5,7 @@ import { Video } from '../config/mongoose';
 import fs from 'fs';
 // path => otra libreria para manejar las rutas de los archivos
 import path from 'path';
+import { CallbackError } from 'mongoose';
 
 export var crearVideo = (req: Request, res: Response) => {
     // si no deseo trabajar como una funcion de respuesta con error, se puede usar las promesas, pero si usas el function ya no usas la promesa y viceversa
@@ -60,16 +61,77 @@ export var subirImagen = (req: any, res: Response) => {
     }
 }
 
-export var devolverImagenPorNombre = (req:Request, res:Response)=>{
-    let {nombre} = req.params;
+export var devolverImagenPorNombre = (req: Request, res: Response) => {
+    let { nombre } = req.params;
     let ruta = `multimedia/${nombre}`
     let rutaDefault = 'multimedia/image.jpg'
     // verifica si existe ese archivo en mi proyecto, entonces retornará un True si existe o un False si no existe
-    if(fs.existsSync(ruta)){
+    if (fs.existsSync(ruta)) {
         // resolve sirve para mostrar un archivo
         // sendFile => sirve para mandar al front un archivo y nada mas que un archivo, no se puede mandar texto adicional
         return res.sendFile(path.resolve(ruta));
-    }else{
+    } else {
         return res.sendFile(path.resolve(rutaDefault));
     }
 }
+
+export var eliminarVideoConImagen = (req: Request, res: Response) => {
+    // BUSCAR EL VIDEO SEGUN SU ID
+    // 1. eliminar el video de la base de datos
+    // 2. buscar la imagen en el servidor
+    // 3. si la encuentro la voy a eliminar del servidor para evitar archivos basuras
+    let { id } = req.params;
+    Video.findByIdAndDelete(id, {}, (error, objVideo: any) => {
+        // si no hay error pero si hay un objeto del video significa que ese video con el id existe en la bd y ya se eliminó
+        if (!error && objVideo) {
+            // eliminar la imagen del servidor 
+            fs.unlink(`multimedia/${objVideo.vid_img}`, (errorEliminacion) => {
+                if (errorEliminacion) {
+                    res.status(500).json({
+                        ok: false,
+                        content: errorEliminacion,
+                        message: 'Se elimino exitosamente de la bd, pero hubo un problema al eliminar el archivo del servidor'
+                    });
+                } else {
+                    res.json({
+                        ok: true,
+                        content: objVideo,
+                        message: 'Se elimino exitosamente el video de la bd'
+                    })
+                }
+            })
+        } else {
+            res.status(500).json({
+                ok: false,
+                content: error,
+                message: 'Hubo un error al eliminar el video de la Bd'
+            })
+        }
+    })
+}
+
+// agregar un comentario
+// HINT: devolver la lista de comentarios y agregar al final....
+export var agregarComentarioPorIdVideo = (req: Request, res: Response) => { 
+    let {id}= req.params;
+    Video.findById(id,(error:CallbackError,resultado:any)=>{
+        if(!error){
+            resultado.vid_coments.push(req.body);
+            resultado.save();
+            res.status(201).json({
+                ok:true,
+                content:resultado,
+                message:'Se agrego exitosamente el comentario'
+            })
+        }
+        else{
+            res.status(500).json({
+                ok:false,
+                content:error,
+                message:'Hubo un error al registrar el comentario'
+            })
+        }
+    })
+}
+
+// hacer un controlador para que se registre el like, solamente puede registrarlo una persona que ya este registrada en la coleccion de usuarios
