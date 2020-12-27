f12a5a64d322d4ac076d5f6f21092fab86a5e305
Eduardo de Rivero <eduardo.derivero@valigo.org>
2020-12-27 18:03:19 -0500

    avance

---
+++ Semana 10/Zapateria/src/controllers/venta.js
@@ -0,0 +1,7 @@
+const {Producto, Venta, Cliente} = require('../config/Mongoose');
+
+// crear venta
+
+// mostrar venta segun dni del cliente
+
+// notificaciones de mercado pago de una venta en especifico
