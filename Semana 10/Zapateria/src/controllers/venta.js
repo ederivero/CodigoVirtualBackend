7c3d37f8879291c8fdaa865554726f0520ad2bd6
Eduardo de Rivero <eduardo.derivero@valigo.org>
2020-12-28 18:43:07 -0500

    avance

--- Semana 10/Zapateria/src/controllers/venta.js
+++ Semana 10/Zapateria/src/controllers/venta.js
@@ -2,18 +2,48 @@ const {Producto, Venta, Cliente} = require('../config/Mongoose');
 const mercadopago = require('../config/MercadoPago');
 // crear la preferencia de mercado pago
 const preferenciaMercadoPago = async(req, res)=>{
-    console.log(req.body);
     // tengo que ver los productos id's y buscarlos en la colecion de producto
     let {productos} = req.body;
+    let items = [];
+    // buscar el cliente y rellenar con los datos de ese cliente el payer y si no existe el cliente indicar que no existe
+    let payer = {
+        name:"Lalo",
+        surname: "Landa",
+        email:"test_user_46542185@testuser.com", // el email tiene que ser este
+        phone:{
+            number:5549737300,
+            area_code:"52"
+        },
+        identification:{
+            type:"dni",
+            number:"22334445"
+        },
+        address:{
+            zip_code : "03940",
+            street_name:"Insurgentes Sur",
+            street_number:1602,
+        }
+      }
     for (const key in productos) {
         console.log(productos[key]);
         try {
             let producto = await Producto.findById(productos[key].productoId);
-            console.log(producto)
+            let item = {
+                id: producto._id,
+                title: producto.productoTitulo,
+                description: producto.productoDescripcion,
+                picture_url: producto.productoImagen,
+                quantity: productos[key].cantidad,
+                currency_id: producto.productoMoneda,
+                unit_price: producto.productoPrecio
+            }
+            items.push(item);
+            // console.log(producto)
         } catch (error) {
             console.log(error)
         }
     }
+    console.log(items);
     // tengo que ver el cliente id y buscarlo
     // OPCIONALMENTE: restringir algunos metodos de pago, cuotas de pago, formas de pago, etc...
     // configurar las url's de respuesta (back_urls)
